49a081da3f56b9c6c0ad7a084652336d445727cb
Vadim Fintisov <fintisov.vadim@gmail.com>
2021-11-15 12:07:16 +0200

    added new script for date

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//****** timer ******
+
 let showTime;
 
 function startTimer(duration, display) {
@@ -18,11 +20,45 @@ function startTimer(duration, display) {
             timer = duration;
             clearInterval(showTime);
         }
+
     }, 1000);
 }
+
+//****** date now ******
+
+const dateNow = document.querySelector("#date-now");
+
+function date(show) {
+    const date = new Date();
+
+    const month = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ]
+
+    let Y = date.getFullYear();
+    let M = month[date.getMonth()];
+    let D = date.getDate();
+
+    show.textContent = `${M} ${D}, ${Y}`;
+}
+
+//********************************
 
 window.onload = function () {
     let tenMinutes = 60 * 10;
     let display = document.querySelector("#time");
     startTimer(tenMinutes, display);
+
+    date(dateNow)
 };
